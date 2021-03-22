ed0ef8eb95e0043de0c1bf1642496cf4665356b2
KasPeeer <77361944+KasPeeer@users.noreply.github.com>
2021-03-23 02:49:20 +0300

    hi

---
+++ commands/Public commands.js
@@ -0,0 +1,11 @@
+const { MessageEmbed } = require("discord.js");
+
+module.exports = {
+  name: "Pub",
+  cooldown: 5,
+  aliases: ["P"],
+  description: "send NSFW Photos in the channel",
+  async execute(message) {
+      const textChannel = message.channel;
+  }
+}
