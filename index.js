22dfffe6e6adddfc0ed34cdd8a5a686e829d7782
KasPeeer <77361944+KasPeeer@users.noreply.github.com>
2021-03-23 09:34:38 +0300

    hi

--- index.js
+++ index.js
@@ -30,6 +30,7 @@ client.on('message', async message => {
     message.react('💙') }
  // ---------------------------------------------------------------------------------------------------- Public Commands
  // ---------------------------------------------------------------------------------------------------- music Commands
+ 
   if(message.content.startsWith(Prefix + `pause`)) {
     if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel to use this command')
     if(!serverQueue) return message.channel.send('There is no music playing')
@@ -91,6 +92,9 @@ client.on('message', async message => {
     message.channel.send(`I have changed the volume to ${args[1]}`)
     return undefined
  } else if(message.content.startsWith(Prefix + `np`)) {
+    const song = {
+        duration: video.duration
+    }
     const serverQueue = queue.get(message.guild.id)
     if(!message.member.voice.channel) return message.channel.send('You need to be in a voice channel to use this command')
     if(!serverQueue) return message.channel.send('There is no music playing')
